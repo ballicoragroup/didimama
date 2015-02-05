a61b81d319ee10ca0b92c98b7961035437fac738
Miguel A. Ballicora <mballic@luc.edu>
2015-02-04 18:38:11 -0600

    [v0.3.4] modified deploy

--- compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-gcc -DNDEBUG -Wall -Wextra -O3 myopt/myopt.c proginfo.c rmsdcmp.c pdb.c fit.c strtools.c main.c -I myopt -o didimama -lm
-
--- version.h
+++ version.h
@@ -1,2 +1,2 @@
-#define VERSION "v0.3.3"
+#define VERSION "v0.3.4"
 
